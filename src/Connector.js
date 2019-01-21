87dd3f7d146e5aa5da2a0860aaa51313cf1afedd
Ramon Ribeiro <rhpr@cesar.org.br>
2019-01-21 17:48:37 -0300

    Add addDevice
    Change-Id: I6700332a7e57a6b9b725618780ff210002821970

--- src/Connector.js
+++ src/Connector.js
@@ -12,19 +12,32 @@ function promisify(client, event, method, ...args) {
 
 class Connector {
   constructor(settings) {
-    this.client = new Client({
-      hostname: settings.hostname,
-      port: settings.port,
-      uuid: settings.uuid,
-      token: settings.token,
+    this.settings = settings;
+    this.client = null;
+    this.clientThings = {};
+  }
+
+  async createConnection(id, token) {
+    const client = new Client({
+      hostname: this.settings.hostname,
+      port: this.settings.port,
+      id,
+      token,
     });
+    await promisify(client, 'ready', client.connect.bind(client));
+    return client;
   }
 
   async start() {
-    return promisify(this.client, 'ready', this.client.connect.bind(this.client));
+    this.client = await this.createConnection(this.settings.uuid, this.settings.token);
   }
 
-  async addDevice(device) { // eslint-disable-line no-empty-function, no-unused-vars
+  async addDevice(device) {
+    const properties = device;
+    properties.type = 'thing';
+    const newDevice = await promisify(this.client, 'registered', this.client.register.bind(this.client), properties);
+    this.clientThings[newDevice.id] = await this.createConnection(newDevice.uuid, newDevice.token);
+    return newDevice;
   }
 
   async removeDevice(id) { // eslint-disable-line no-empty-function, no-unused-vars
