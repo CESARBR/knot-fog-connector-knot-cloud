aaa4da7a02fb85b736d33f7c0518a476cc8e41da
Ramon Ribeiro <rhpr@cesar.org.br>
2019-01-21 17:48:37 -0300

    Add addDevice
    Change-Id: I6700332a7e57a6b9b725618780ff210002821970

--- src/Connector.js
+++ src/Connector.js
@@ -12,6 +12,8 @@ function promisify(client, event, method, ...args) {
 
 class Connector {
   constructor(settings) {
+    this.settings = settings;
+    this.clientThings = {};
     this.client = new Client({
       hostname: settings.hostname,
       port: settings.port,
@@ -20,11 +22,27 @@ class Connector {
     });
   }
 
+  async createConnection(uuid, token) {
+    const client = new Client({
+      hostname: this.settings.hostname,
+      port:  this.settings.port,
+      uuid:  uuid,
+      token: token,
+    });
+    await promisify(client, 'ready', client.connect.bind(client));
+    return client;
+  }
+
   async start() {
     return promisify(this.client, 'ready', this.client.connect.bind(this.client));
   }
 
-  async addDevice(device) { // eslint-disable-line no-empty-function, no-unused-vars
+  async addDevice(device) {
+    const properties = device;
+    properties.type = 'thing';
+    const newDevice = await promisify(this.client, 'registered', this.client.register.bind(this.client), properties);
+    this.clientThings[newDevice.id] = this.createConnection(newDevice.uuid, newDevice.token);
+    return newDevice;
   }
 
   async removeDevice(id) { // eslint-disable-line no-empty-function, no-unused-vars
