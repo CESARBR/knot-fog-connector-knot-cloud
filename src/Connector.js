5be8cc84ccfab393a99c7334640f97e2c8a16059
João Neto <joao.alexandre@cesar.org.br>
2019-02-05 09:49:58 -0300

    Add updateSchema
    Change-Id: Iec5f60bb310dd0528d9306e4027b45e4f131207a

--- src/Connector.js
+++ src/Connector.js
@@ -10,27 +10,48 @@ function promisify(client, event, method, ...args) {
   });
 }
 
+async function loadDevices(client) {
+  const devices = await promisify(client, 'devices', client.getDevices.bind(client), { type: 'thing' });
+  return Promise.all(devices.map(async device => ({
+    id: device.id,
+    token: await promisify(client, 'created', client.createSessionToken.bind(client), device.uuid),
+  })));
+}
+
 class Connector {
   constructor(settings) {
+    this.settings = settings;
     this.client = new Client({
       hostname: settings.hostname,
       port: settings.port,
-      uuid: settings.uuid,
+      id: settings.uuid,
       token: settings.token,
     });
+
+    this.things = [];
   }
 
   async start() {
-    return promisify(this.client, 'ready', this.client.connect.bind(this.client));
+    await promisify(this.client, 'ready', this.client.connect.bind(this.client));
+    this.things = await loadDevices(this.client);
   }
 
   async addDevice(device) {
     const properties = device;
     properties.type = 'thing';
-    return promisify(this.client, 'registered', this.client.register.bind(this.client), properties);
+    const newDevice = await promisify(this.client, 'registered', this.client.register.bind(this.client), properties);
+    this.things.push({
+      id: newDevice.id,
+      token: await promisify(this.client, 'created', this.client.createSessionToken.bind(this.client), newDevice.uuid),
+    });
+    return newDevice;
   }
 
   async removeDevice(id) {
+    const index = this.things.indexOf(this.things.find(device => device.id === id));
+    if (index > -1) {
+      this.things.splice(index, 1);
+    }
     return promisify(this.client, 'unregistered', this.client.unregister.bind(this.client), id);
   }
 
@@ -43,7 +64,17 @@ class Connector {
   async publishData(id, dataList) { // eslint-disable-line no-empty-function, no-unused-vars
   }
 
-  async updateSchema(id, schemaList) { // eslint-disable-line no-empty-function, no-unused-vars
+  async updateSchema(id, schemaList) {
+    const thing = this.things.find(device => device.id === id);
+    const thingClient = new Client({
+      hostname: this.settings.hostname,
+      port: this.settings.port,
+      id: thing.id,
+      token: thing.token,
+    });
+
+    await promisify(thingClient, 'ready', thingClient.connect.bind(thingClient));
+    return promisify(thingClient, 'schema', thingClient.updateSchema.bind(thingClient), schemaList);
   }
 
   async updateProperties(id, properties) { // eslint-disable-line no-empty-function, no-unused-vars
