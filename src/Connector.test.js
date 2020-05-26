01f716ec227664dee6e6ee241b836c95a7321995
Esdras Júnior <eblsj@cesar.org.br>
2020-05-26 12:48:05 -0300

    tests: Add test cases for listenToCommands method

--- src/Connector.test.js
+++ src/Connector.test.js
@@ -3,8 +3,23 @@ import Connector from './Connector';
 
 jest.mock('@cesarbr/knot-cloud-sdk-js-amqp');
 
+const mockThing = {
+  id: 'abcdef1234568790',
+  name: 'my-device',
+  schema: [
+    {
+      sensorId: 0,
+      typeId: 65521,
+      valueType: 3,
+      unit: 0,
+      name: 'bool-sensor',
+    },
+  ],
+};
+
 const errors = {
   connectClient: 'fail to connect to AMQP channel',
+  listenToCommands: 'fail to list registered things from cloud',
 };
 
 describe('Connector', () => {
@@ -45,5 +60,29 @@ describe('Connector', () => {
     }
     expect(clientMocks.mockConnect).toHaveBeenCalled();
     expect(error).toBe(errors.connectClient);
+  });
+
+  test('listenToCommands: should start listeners on registered devices when there is no error', async () => {
+    const registeredDevices = [mockThing];
+    const client = new Client({ registeredDevices });
+    const connector = new Connector(client);
+    await connector.listenToCommands();
+    expect(connector.devices).toEqual([mockThing.id]);
+    expect(clientMocks.mockGetDevices).toHaveBeenCalled();
+    expect(clientMocks.mockOn).toHaveBeenCalledTimes(
+      registeredDevices.length * 2
+    );
+  });
+
+  test('listenToCommands: should fail to subscribe on commands when unable to get things from cloud', async () => {
+    const client = new Client({ getDevicesErr: errors.listenToCommands });
+    const connector = new Connector(client);
+    let error;
+    try {
+      await connector.listenToCommands();
+    } catch (err) {
+      error = err.message;
+    }
+    expect(error).toBe(errors.listenToCommands);
   });
 });
